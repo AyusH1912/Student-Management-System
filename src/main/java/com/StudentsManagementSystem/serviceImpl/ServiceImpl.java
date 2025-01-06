9e061bb9c9fddbb04742052163dde6f2283cfe32
AyusH1912 <aayushsrivastava614@gmail.com>
2025-01-06 17:40:52 +0530

    initial commit

---
+++ src/main/java/com/StudentsManagementSystem/serviceImpl/ServiceImpl.java
@@ -0,0 +1,38 @@
+package com.StudentsManagementSystem.serviceImpl;
+
+import com.StudentsManagementSystem.entity.Student;
+import com.StudentsManagementSystem.repository.StudentRepository;
+import com.StudentsManagementSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ServiceImpl implements StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Override
+    public List<Student> getAllStudents() {
+        List<Student> list = studentRepository.findAll();
+        return list;
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getById(int id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        studentRepository.deleteById(id);
+    }
+
+
+}
